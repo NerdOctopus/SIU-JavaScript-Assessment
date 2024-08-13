545f0f81f28198db228029e2753220e2a3aef396
NerdOctopus <37950583+NerdOctopus@users.noreply.github.com>
2024-08-13 15:21:34 -0500

    Update pb2.js
    notepad is annoying

--- problems/pb2.js
+++ problems/pb2.js
@@ -1,5 +1,23 @@
 // declare and define function here
-
+function mostFrequentChar(string){
+	var keyCount = {};
+	var mostKey = '';
+	for(var i=0; i<string.length; i++){
+		var key = string[i];
+		if(!keyCount[key]){
+		keyCount[key]=0;
+		}
+		keyCount[key]++;
+		if(mostKey == '' || keyCount[key] > keyCount[mostKey]){
+			mostKey = key;
+		}else if(keyCount[key] = keyCount[mostKey]){
+			if(string.indexOf(mostKey) > string.indexOf(key)){
+				mostKey = key;
+			}
+		}
+	}
+	console.log(mostKey);
+}
 
 // test cases
 // mostFrequentChar('bookeeper'); // -> 'e'

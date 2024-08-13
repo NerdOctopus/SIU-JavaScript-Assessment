d3718d2cdd4057f777bebbc4b696207d56726369
NerdOctopus <37950583+NerdOctopus@users.noreply.github.com>
2024-08-13 15:19:30 -0500

    Update pb1.js

--- problems/pb1.js
+++ problems/pb1.js
@@ -1,5 +1,7 @@
 // declare and define function here
-
+function replaceSpaces(string, char) {
+	return(string.replace(/ /g, char));
+}
 
 // test cases
 // replaceSpaces('Today is a good day', '-'); // -> 'Today-is-a-good-day'

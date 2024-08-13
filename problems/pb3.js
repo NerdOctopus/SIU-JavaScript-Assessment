ff41c907c6a00190be5bd1b3cc88b0472a3feb6d
NerdOctopus <37950583+NerdOctopus@users.noreply.github.com>
2024-08-13 15:55:22 -0500

    Update pb3.js
    slightly late but this became a point of pride

--- problems/pb3.js
+++ problems/pb3.js
@@ -1,6 +1,26 @@
 // declare and define function here
-
+function compress(string){
+	var count = 1;
+	var char = '';
+	var result = '';
+	var answer = '';
+	for (var i = 0; i < string.length; i++){
+		char = string[i]
+		if (string[i] == string[i+1]){
+			count++;
+		} else {
+			if (count > 1){
+				result = count + char
+				count = 1
+				answer = answer + result
+			} else {
+				answer = answer + char
+			}
+		} 
+	}
+return answer
+}
 
 // test cases
-// compress('ccaaatsss'); // -> '2c3at3s'
-// compress('ssssbbz'); // -> '4s2bz'
+compress('ccaaatsss'); // -> '2c3at3s'
+compress('ssssbbz'); // -> '4s2bz'
